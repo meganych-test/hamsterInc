a3a16e6ecd3cffe93aca6cf700b8f2c166a3fcf6
meganych-test <110050549+meganych-test@users.noreply.github.com>
2024-06-23 13:08:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-window.onload = function() {
-    const desktopMessage = document.getElementById('desktop-message');
-    const bgElement = document.querySelector('.bg');
-    
-    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-        // Desktop device
-        desktopMessage.style.display = 'flex';
-        bgElement.style.display = 'none';
-    } else {
-        // Mobile device
-        desktopMessage.style.display = 'none';
-        bgElement.style.display = 'block';
-        
-        const tg = window.Telegram.WebApp;
-        
-        // Expand the Web App to full height
-        tg.expand();
-        // Set the background color to black
-        tg.setBackgroundColor('#000000');
-        // Set the header color (you might want to adjust this)
-        tg.setHeaderColor('#000000');
-        // Additional initialization code can go here
-    }
-};
+        window.onload = function() {
+            if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+                document.getElementById('desktop-message').style.display = 'block';
+                document.querySelector('.bg').style.display = 'none';
+            } else {
+                const tg = window.Telegram.WebApp;
+                
+                // Expand the Web App to full height
+                tg.expand();
+                // Set the background color to black
+                tg.setBackgroundColor('#000000');
+                // Set the header color (you might want to adjust this)
+                tg.setHeaderColor('#000000');
+                // Additional initialization code can go here
+            }
+        };
