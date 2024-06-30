eb53da02f056d8631fec98bfb41bee66eb2ee75d
meganych-test <110050549+meganych-test@users.noreply.github.com>
2024-06-30 15:29:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
+<script>
+    window.onload = function() {
+        const tg = window.Telegram.WebApp;
+        
+        // Expand the Web App to full height
+        tg.expand();
+
+        // Set the background color to black
+        tg.setBackgroundColor('#000000');
+
+        // Set the header color (you might want to adjust this)
+        tg.setHeaderColor('#000000');
+
+        // Additional initialization code can go here
+    };
+</script>
